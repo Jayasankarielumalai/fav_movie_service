ae3e79a07e84de02e05eab7e726c5fb02478a988
jayasankari <jayasankarielumalai@gmail.com>
2024-08-15 22:27:28 +0530

    created all layer need to do method impl..

--- src/main/java/com/niit/FavouriteMovieService/controller/FavouriteMovieController.java
+++ src/main/java/com/niit/FavouriteMovieService/controller/FavouriteMovieController.java
@@ -2,6 +2,7 @@ package com.niit.FavouriteMovieService.controller;
 
 import com.niit.FavouriteMovieService.domain.User;
 import com.niit.FavouriteMovieService.exception.UserAlreadyExistException;
+import com.niit.FavouriteMovieService.exception.UserNotFoundException;
 import com.niit.FavouriteMovieService.service.IFavouriteMovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,13 +19,26 @@ public class FavouriteMovieController {
         this.iFavouriteMovieService = iFavouriteMovieService;
     }
     @PostMapping("/register")
-    public ResponseEntity register(@RequestBody User user) throws UserAlreadyExistException{
+    public ResponseEntity registerUser(@RequestBody User user) throws UserAlreadyExistException{
     try {
-        User updatedUser=iFavouriteMovieService.registerUser(user);
-        responseEntity=new ResponseEntity<>(updatedUser, HttpStatus.CREATED);
+        User registeredUser=iFavouriteMovieService.registerUser(user);
+        responseEntity=new ResponseEntity<>(registeredUser, HttpStatus.CREATED);
     } catch (UserAlreadyExistException e) {
         throw new UserAlreadyExistException();
-    }catch (Exception e){
+    } catch (Exception e){
+        responseEntity=new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    return responseEntity;
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity updateUser(@RequestBody User user) throws UserNotFoundException{
+    try{
+        User updatedUser=iFavouriteMovieService.updateUser(user);
+        responseEntity=new ResponseEntity<>(updatedUser,HttpStatus.OK);
+    } catch (UserNotFoundException e) {
+        throw new UserNotFoundException();
+    } catch (Exception e){
         responseEntity=new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
     }
     return responseEntity;
